1a1f844f02b531a27887d798581c4feb2303c643
fzhang07 <fzhang07@gmail.com>
2016-10-02 20:03:54 -0400

    Revert "fixed hover stuff, actually committed this time"
    This reverts commit d75a868b2add075630a56f354217649adb67a60a.

--- project3/fifteen.js
+++ project3/fifteen.js
@@ -11,7 +11,6 @@ function init() {
         boxes.item(i).style.backgroundPosition = backgroundImagePos(i);
         boxes.item(i).onclick = swap;
     }
-    addHoverClassToAdjacent();
     var shuffleButton = document.getElementById("shufflebutton");
     shuffleButton.onclick = shuffle;
 }
@@ -27,20 +26,6 @@ function swap(e) {
         emptySpot = pos;
     }
     console.log("Empty Spot: " + emptySpot);
-    addHoverClassToAdjacent();
-}
-
-function addHoverClassToAdjacent() {
-    var boxes = puzzlearea.children;
-    var adjPos = generateAdjacentToEmptySpots();
-    var i = 0;
-    for (; i < boxes.length; i++) {
-        boxes.item(i).className = "box";
-    }
-    for (i = 0; i < adjPos.length; i++) {
-        var adjBox = document.getElementById("pos" + adjPos[i]);
-        adjBox.className = "box boxHover";
-    }
 }
 
 //0 indexed position
@@ -81,9 +66,7 @@ function shuffle(e) {
     var previousPos = emptySpot;
     (function shuffleIteration() {
         var positions = generateAdjacentToEmptySpots();
-        positions = positions.filter(function (pos) {
-            return pos != previousPos
-        });
+        positions = positions.filter(function(pos){return pos != previousPos});
         var randIndex = Math.floor(Math.random() * positions.length);
         var swapPos = positions[randIndex];
         console.log("Swap Pos " + swapPos);
@@ -92,7 +75,8 @@ function shuffle(e) {
         console.log(divToClick);
         divToClick.click();
         i++;
-        if (i < 100) {
+        if(i < 100) 
+        {
             setTimeout(shuffleIteration, 50);
         }
     })();
